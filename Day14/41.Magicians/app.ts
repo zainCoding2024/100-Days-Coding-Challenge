b698c6a740f18405ae51d7eed843776a13021842
zain <zainmd.htm@gmail.com>
2024-07-02 09:55:34 +0500

    Add files

---
+++ Day14/41.Magicians/app.ts
@@ -0,0 +1,15 @@
+//Exercise 41: Magicians
+// Define an array of magician names
+const magicians:string[] =['Ahmed','jawad','nooman'];
+
+// Define a function to return the magician names
+function  show_magician(magicians:string[]){
+    //return magician;
+    for(const magician of magicians){
+         console.log(magician)
+    }
+}
+// Call the function to get the magician names
+//const magician = show_magician(magicians)
+// Log the magician names.
+          show_magician(magicians)
---
+++ Day98/ReadMe.md
@@ -0,0 +1,13 @@
+
+Chapter 15
+
+Adding Authentication
+
+https://nextjs.org/learn/dashboard-app/adding-authentication
+
+
+
+
+In this chapter i faced a problem there is a login issue it's not going to login but currently i am going ahead with this issue.
+
+
